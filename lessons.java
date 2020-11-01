0bc1123b529e57c411902592bbb754ee0fd6f8f2
funny-walnut <59824970+funny-walnut@users.noreply.github.com>
2020-11-01 11:59:28 +0300

    Update lessons.java

--- lessons.java
+++ lessons.java
@@ -1,5 +1,28 @@
 int [] MadMax(int N, int [] Tele)
     {
+        boolean unic;
+        Random random = new Random();
+        Tele[0] = random.nextInt(256);
+        System.out.println(Tele[0]);
+        for (int i = 1; i<N; i++) {
+            do {
+                unic = true;
+                int number = random.nextInt(256);
+                for (int j = 0; j < i; j++) {
+                    if (Tele[j] == number) {
+                        unic = false;
+                        break;
+                    }else {
+                        Tele [i] = number;
+                    }
+
+
+                }System.out.println("i = " + i);
+                System.out.print(Tele[i] +" ");
+            } while (!unic);
+        }
+
+
 
         for (int a = 1;a <Tele.length; a++)
         {
